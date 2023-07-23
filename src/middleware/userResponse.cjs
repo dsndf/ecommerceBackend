952d2e1cbb2e212d5656302dd0c90560f561ec28
Gaurav Jain <134189121+dsndf@users.noreply.github.com>
2023-07-23 14:33:09 +0530

    Update userResponse.cjs

--- src/middleware/userResponse.cjs
+++ src/middleware/userResponse.cjs
@@ -5,7 +5,9 @@ const userResponse = (doc,token,req,res)=>{
    
     res.cookie('token',token,{
         httpOnly:true,
-        expires: new Date(Date.now()+process.env.COOKIE*24*60*60*1000)
+        expires: new Date(Date.now()+process.env.COOKIE*24*60*60*1000),
+            secure:true,
+        sameSite:"none"
     });
 
     res.send({
